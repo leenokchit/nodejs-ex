41802606c352a4d341cd926346eae8fdbc883d55
chitlee <leenokchit@gmail.com>
2018-02-08 14:36:48 +0800

    add auth

---
+++ functions.js
@@ -0,0 +1,110 @@
+var bcrypt = require('bcryptjs'),
+    Q = require('q');
+    //config = require('./config.js'); //config file contains all tokens and other private info
+
+
+////get mongourl
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
+
+if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+      mongoUser = process.env[mongoServiceName + '_USER'];
+      //mongoUser = "admin";
+
+  if (mongoHost && mongoPort && mongoDatabase) {
+    mongoURLLabel = mongoURL = 'mongodb://';
+    if (mongoUser && mongoPassword) {
+      mongoURL += mongoUser + ':' + mongoPassword + '@';
+    }
+    // Provide UI label that excludes user id and pw
+    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+
+  }
+}
+////
+// MongoDB connection information
+var mongodbUrl = 'mongodb://' + 'localhost' + ':27117/users';
+var mongodbUrl = mongoURL;
+var MongoClient = require('mongodb').MongoClient
+
+//used in local-signup strategy
+exports.localReg = function (username, password) {
+  var deferred = Q.defer();
+  
+  MongoClient.connect(mongodbUrl, function (err, db) {
+    var collection = db.collection('localUsers');
+
+    //check if username is already assigned in our database
+    collection.findOne({'username' : username})
+      .then(function (result) {
+        if (null != result) {
+          console.log("USERNAME ALREADY EXISTS:", result.username);
+          deferred.resolve(false); // username exists
+        }
+        else  {
+          var hash = bcrypt.hashSync(password, 8);
+          var user = {
+            "username": username,
+            "password": hash,
+            "avatar": "http://placepuppy.it/images/homepage/Beagle_puppy_6_weeks.JPG"
+          }
+
+          console.log("CREATING USER:", username);
+        
+          collection.insert(user)
+            .then(function () {
+              db.close();
+              deferred.resolve(user);
+            });
+        }
+      });
+  });
+
+  return deferred.promise;
+};
+
+
+//check if user exists
+    //if user exists check if passwords match (use bcrypt.compareSync(password, hash); // true where 'hash' is password in DB)
+      //if password matches take into website
+  //if user doesn't exist or password doesn't match tell them it failed
+exports.localAuth = function (username, password) {
+  var deferred = Q.defer();
+
+  MongoClient.connect(mongodbUrl, function (err, db) {
+    var collection = db.collection('localUsers');
+
+    collection.findOne({'username' : username})
+      .then(function (result) {
+        if (null == result) {
+          console.log("USERNAME NOT FOUND:", username);
+
+          deferred.resolve(false);
+        }
+        else {
+          var hash = result.password;
+
+          console.log("FOUND USER: " + result.username);
+
+          if (bcrypt.compareSync(password, hash)) {
+            deferred.resolve(result);
+          } else {
+            console.log("AUTHENTICATION FAILED");
+            deferred.resolve(false);
+          }
+        }
+
+        db.close();
+      });
+  });
+
+  return deferred.promise;
+}
--- server.js
+++ server.js
@@ -1,12 +1,98 @@
 //  OpenShift sample Node application
 var express = require('express'),
     app     = express(),
-    morgan  = require('morgan');
-    
+    morgan  = require('morgan'),
+    exphbs  = require('express-handlebars'),
+    passport = require('passport');
+
+var funct = require('./functions.js');
+
 Object.assign=require('object-assign')
+
+//===============PASSPORT=================
+
+// Passport session setup.
+passport.serializeUser(function(user, done) {
+  console.log("serializing " + user.username);
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  console.log("deserializing " + obj);
+  done(null, obj);
+});
+
+// Use the LocalStrategy within Passport to login users.
+passport.use('local-signin', new LocalStrategy(
+  {passReqToCallback : true}, //allows us to pass back the request to the callback
+  function(req, username, password, done) {
+    funct.localAuth(username, password)
+    .then(function (user) {
+      if (user) {
+        console.log("LOGGED IN AS: " + user.username);
+        req.session.success = 'You are successfully logged in ' + user.username + '!';
+        done(null, user);
+      }
+      if (!user) {
+        console.log("COULD NOT LOG IN");
+        req.session.error = 'Could not log user in. Please try again.'; //inform user could not log them in
+        done(null, user);
+      }
+    })
+    .fail(function (err){
+      console.log(err.body);
+    });
+  }
+));
+
+// Simple route middleware to ensure user is authenticated.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  req.session.error = 'Please sign in!';
+  res.redirect('/signin');
+}
+//===============PASSPORT END==============
+
 
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
+
+////
+// Configure Express
+app.use(express.logger());
+app.use(express.cookieParser());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.session({ secret: 'supernova' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function(req, res, next){
+  var err = req.session.error,
+      msg = req.session.notice,
+      success = req.session.success;
+
+  delete req.session.error;
+  delete req.session.success;
+  delete req.session.notice;
+
+  if (err) res.locals.error = err;
+  if (msg) res.locals.notice = msg;
+  if (success) res.locals.success = success;
+
+  next();
+});
+
+app.use(app.router);
+
+// Configure express to use handlebars templates
+var hbs = exphbs.create({
+    defaultLayout: 'main',
+});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+////
+
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
@@ -20,7 +106,7 @@ if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
       mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
       mongoPassword = process.env[mongoServiceName + '_PASSWORD']
       mongoUser = process.env[mongoServiceName + '_USER'];
-      mongoUser = "admin";
+      //mongoUser = "admin";
 
   if (mongoHost && mongoPort && mongoDatabase) {
     mongoURLLabel = mongoURL = 'mongodb://';
@@ -57,6 +143,29 @@ var initDb = function(callback) {
   });
 };
 
+// Use the LocalStrategy within Passport to Register/"signup" users.
+passport.use('local-signup', new LocalStrategy(
+  {passReqToCallback : true}, //allows us to pass back the request to the callback
+  function(req, username, password, done) {
+    funct.localReg(username, password)
+    .then(function (user) {
+      if (user) {
+        console.log("REGISTERED: " + user.username);
+        req.session.success = 'You are successfully registered and logged in ' + user.username + '!';
+        done(null, user);
+      }
+      if (!user) {
+        console.log("COULD NOT REGISTER");
+        req.session.error = 'That username is already in use, please try a different one.'; //inform user could not log them in
+        done(null, user);
+      }
+    })
+    .fail(function (err){
+      console.log(err.body);
+    });
+  }
+));
+
 app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
@@ -92,6 +201,41 @@ app.get('/pagecount', function (req, res) {
     res.send('{ pageCount: -1 }');
   }
 });
+
+////router
+//displays our homepage
+app.get('/home', function(req, res){
+  res.render('home', {user: req.user});
+});
+
+//displays our signup page
+app.get('/signin', function(req, res){
+  res.render('signin');
+});
+
+//sends the request through our local signup strategy, and if successful takes user to homepage, otherwise returns then to signin page
+app.post('/local-reg', passport.authenticate('local-signup', {
+  successRedirect: '/home',
+  failureRedirect: '/signin'
+  })
+);
+
+//sends the request through our local login/signin strategy, and if successful takes user to homepage, otherwise returns then to signin page
+app.post('/login', passport.authenticate('local-signin', { 
+  successRedirect: '/home',
+  failureRedirect: '/signin'
+  })
+);
+
+//logs user out of site, deleting them from the session, and returns to homepage
+app.get('/logout', function(req, res){
+  var name = req.user.username;
+  console.log("LOGGIN OUT " + req.user.username)
+  req.logout();
+  res.redirect('/home');
+  req.session.notice = "You have successfully been logged out " + name + "!";
+});
+////
 
 // error handling
 app.use(function(err, req, res, next){
