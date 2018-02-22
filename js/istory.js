44c34609a3d98230c6b9a2f77bfdb20948ba93bf
chitlee <leenokchit@gmail.com>
2018-02-22 18:05:50 +0800

    test vue

--- js/istory.js
+++ js/istory.js
@@ -1,7 +1,37 @@
 $(function() {
-    function getRandomSize(min, max) {
-        return Math.round(Math.random() * (max - min) + min);
-    } 
+    app_gallery = new Vue({
+        el: '#app-gallery',
+        data: {
+            test: "abcdefg",
+          seen: true,
+          isFirstTimeEnter: false,
+          bucketlist: []
+        },
+        mounted: function (){
+            // $.ajax({
+            //     type: 'get',
+            //     url: '/listBuckets',
+            //     success: function (data) {
+            //         console.log(data);
+            //         data.forEach(function(result){
+            //             app_gallery.bucketlist.push({id: result.id, name:result.name});
+            //         });
+            //     }
+            //   })
+        },
+        methods: {
+            showapp: function () {
+              this.seen = true;
+              $("#my_nanogallery2").nanogallery2('refresh');
+            },
+            addBucket: function(name){
+                this.bucketlist.push(name);
+            }
+        }
+      })
+
+
+
     function photoUpload(){
         var dataform = new FormData($("#photoUpload")[0]);
 
@@ -24,37 +54,26 @@ $(function() {
         photoUpload();
     });
 
-  
-  var allImages = "";
-  
-  for (var i = 0; i < 0; i++) {
-    var width = getRandomSize(200, 400);
-    var height =  getRandomSize(200, 400);
-    allImages += '<img src="https://placekitten.com/'+width+'/'+height+'" alt="pretty kitty">';
-  }
-  
-  $('#photos').append(allImages);
 
-  $.ajax({
-    type: 'get',
-    url: '/listBuckets',
-    success: function (data) {
-        console.log(data);
-    }
-  })
 
 
 
 
   $("#my_nanogallery2").nanogallery2({
-    thumbnailHeight:  150,
-    thumbnailWidth:   150,
+    thumbnailHeight:  200,
+    thumbnailWidth:   "auto",
+    thumbnailBorderVertical: 0,
+    thumbnailBorderHorizontal: 0,
+    thumbnailAlignment: "center",
+    thumbnailLabel: {
+        "display": false
+      },
     itemsBaseURL:     'https://storage.googleapis.com/',
   
     items: [
-        { src: 'istory-bucket/21817fe5-2e16-4bcc-85a1-910687ecbee9.png', srct: 'thumbnail-istory-bucket/thumbnail-21817fe5-2e16-4bcc-85a1-910687ecbee9.png', title: 'Title Image 1' },
-        { src: 'istory-bucket/52a3acf9-d8a8-4d64-a71c-8f7071451c84.png', srct: 'thumbnail-istory-bucket/thumbnail-52a3acf9-d8a8-4d64-a71c-8f7071451c84.png', title: 'Title Image 2' },
-        { src: 'istory-bucket/5fcace78-0586-4a32-be44-02e4d932af91.png', srct: 'thumbnail-istory-bucket/thumbnail-5fcace78-0586-4a32-be44-02e4d932af91.png', title: 'Title Image 3' }
+        //{ src: 'istory-bucket/21817fe5-2e16-4bcc-85a1-910687ecbee9.png', srct: 'thumbnail-istory-bucket/thumbnail-21817fe5-2e16-4bcc-85a1-910687ecbee9.png', title: 'Title Image 1' },
+        //{ src: 'istory-bucket/52a3acf9-d8a8-4d64-a71c-8f7071451c84.png', srct: 'thumbnail-istory-bucket/thumbnail-52a3acf9-d8a8-4d64-a71c-8f7071451c84.png', title: 'Title Image 2' },
+        //{ src: 'istory-bucket/5fcace78-0586-4a32-be44-02e4d932af91.png', srct: 'thumbnail-istory-bucket/thumbnail-5fcace78-0586-4a32-be44-02e4d932af91.png', title: 'Title Image 3' }
     ]
   });
   
@@ -68,7 +87,7 @@ $(function() {
     var newItem=NGY2Item.New(instance, 'new berlin ' + ID, 'my desc', ID, albumID, 'image', '' );
    
     // define thumbnail
-    newItem.thumbSet('http://nanogallery2.nanostudio.org/samples/berlin1t.jpg', 320, 212); // w,h
+    newItem.thumbImg('http://nanogallery2.nanostudio.org/samples/berlin1t.jpg', 320, 212); // w,h
     // define URL to image
     newItem.src='http://nanogallery2.nanostudio.org/samples/berlin1.jpg';
      
